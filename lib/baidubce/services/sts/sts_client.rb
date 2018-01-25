3e99ab1ad86c7a5f560042ef89b89916866749af
xiaoyong <xiaoyong@baidu.com>
2018-01-25 19:02:58 +0800

    Add sts client and samples
    Change-Id: Ifecf88e007e21a8473521a1960ddd9e909c4487c

---
+++ lib/baidubce/services/sts/sts_client.rb
@@ -0,0 +1,45 @@
+# Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the
+# License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
+# This module provides a client for STS.
+
+require 'json'
+
+require_relative '../../bce_base_client'
+require_relative '../../utils'
+require_relative '../../bce'
+require_relative '../../http/http_methods'
+require_relative '../../http/http_headers'
+require_relative '../../http/http_content_types'
+
+module Baidubce
+    module Services
+
+        class StsClient < Baidubce::BceBaseClient
+
+            STS_URL_PREFIX = "/";
+            GET_SESSION_TOKEN_VERSION = "v1";
+            GET_SESSION_TOKEN_PATH = "sessionToken";
+
+            def get_session_token(acl, duration_seconds=nil)
+                params = {}
+                params = { durationSeconds: duration_seconds } unless duration_seconds.nil?
+                headers = { Baidubce::Http::CONTENT_TYPE => Baidubce::Http::JSON }
+                body = acl.to_json
+                path = Baidubce::Utils.append_uri(STS_URL_PREFIX, GET_SESSION_TOKEN_VERSION, GET_SESSION_TOKEN_PATH)
+                @http_client.send_request(@config, @signer, Baidubce::Http::POST, path, body, headers, params)
+            end
+
+        end
+    end
+end
+
---
+++ samples/baidubce/sts_sample.rb
@@ -0,0 +1,60 @@
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+# Samples for sts client.
+
+$LOAD_PATH.unshift(File.expand_path("../../../lib", __FILE__))
+require 'baidubce/auth/bce_credentials'
+require 'baidubce/services/sts/sts_client'
+require 'baidubce/bce_client_configuration'
+
+credentials = Baidubce::Auth::BceCredentials.new(
+    "535bebda1b894059bbe33cbedda8582e",
+    "a084c2f4e38b459f81da71409826cedb"
+)
+
+conf = Baidubce::BceClientConfiguration.new(
+    credentials,
+    "nmg02-bce-test6.nmg02.baidu.com:8586"
+)
+
+sts_client = Baidubce::Services::StsClient.new(conf)
+
+# bucket_name = "ruby-test-bucket"
+
+def demo(msg)
+  puts "--------- #{msg} --------"
+  puts
+  yield
+  puts "----------- end --------------"
+  puts
+end
+
+demo 'sts client' do
+    acl = {
+            id: '8c47a952db4444c5a097b41be3f24c94',
+            accessControlList: [
+                {
+                    eid: 'shj',
+                    service: 'bce:bos',
+                    region: 'bj',
+                    effect: 'Allow',
+                    resource: ["bos-demo/*"],
+                    permission: ["READ"]
+                }
+            ]
+    }
+
+    # puts sts_client.get_session_token(acl, 1024)
+    puts sts_client.get_session_token(acl)
+    # durationSeconds为非int值，会使用默认的12小时作为失效时间
+    # puts sts_client.get_session_token(acl, "test")
+end
